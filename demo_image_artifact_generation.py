a085ad0ab1c90625c39b75bb3aa2a9b9a03f0bbe
hjkim <hjkim1568@naver.com>
2023-12-18 10:56:55 +0900

    add lenna image

--- demo_image_artifact_generation.py
+++ demo_image_artifact_generation.py
@@ -1,13 +1,17 @@
+#%% image check
+
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-#%% image check
 img = plt.imread("./lenna.png")
+img = np.mean(img, axis=2, keepdims=True)
 sz = img.shape
 cmap = "gray" if sz[2] == 1 else None
-
+print(sz)
 plt.imshow(np.squeeze(img), cmap=cmap, vmin=0, vmax=1)
 plt.title("Ground Truth")
 plt.show()
 
+
+# %%
